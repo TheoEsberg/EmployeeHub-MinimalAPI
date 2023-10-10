27e9a1f6bbf706276f713e613fa4836a7a69d9d9
Lykrat <emil.treptow@outlook.com>
2023-10-10 11:08:36 +0200

    Added a IRepo and EmployeeRepo

---
+++ Services/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeHub_MinimalAPI.Services
+{
+	public interface IRepository<T>
+	{
+		Task<IEnumerable<T>> GetAllAsync();
+		Task<T> GetAsync(int id);
+		Task<T> CreateAsync(T entity);
+		Task<T> UpdateAsync(T entity);
+		Task<T> DeleteAsync(int id);
+	}
+}
