be72a6ce57a8034e147ea60c1574221a40a5552a
Theo Esberg <esbergtheo@gmail.com>
2023-10-25 09:52:41 +0200

    Added UsedLeaveDays

---
+++ Models/UsedLeaveDays.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeHub_MinimalAPI.Models
+{
+	public class UsedLeaveDays
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public int EmployeeId { get; set; }
+		public ICollection<Employee> Employees { get; set;}
+
+		[Required]
+		public int LeaveTypeId { get; set; }
+		public ICollection<LeaveType> LeaveTypes { get; set; }
+
+		[Required]
+		public int Days { get; set; }
+	}
+}
